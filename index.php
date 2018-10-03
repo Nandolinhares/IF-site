e8029ca9b90cee75feba21e5cad3bb35a3ae6886
Fernando Viana <fernandolinhares1920@globo.com>
2018-10-02 23:28:38 -0300

    Ajustado parte do menu e introduzido o slide

--- index.php
+++ index.php
@@ -16,13 +16,53 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<!-- Normalize CSS-->
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+
 	<!-- Bootstrap CSS-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<!-- Meu CSS -->
+	<link rel="stylesheet" type="text/css" href="main.css">
 
 </head>
 <body>
 
-	<h1>Invasão Fantástica</h1>
+	<header>
+		<section class="container-flex">
+			<div class="row nav-fantastica">
+				<div class="col-4 col-sm-3 col-md-2">
+					<img class="header-left" src="photos/header-left.png">
+				</div>
+				<div class="col-8 col-sm-9 col-md-4">
+					<img id="header-right" src="photos/header-right.png">
+				</div>
+			</div>
+		</section>
+	</header>
+	<section>
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="photos/slide1.png" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+	</section>
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
