917e5e4ece963c8eb8532aca3786ac09d0c9ec3f
Fernando Viana <fernandolinhares1920@globo.com>
2018-10-07 23:59:35 -0300

    Finalizada parte da página Beco dos Artst

--- beco.php
+++ beco.php
@@ -81,7 +81,61 @@
 	<section class="beco">
 		<div class="container">
 			<img src="photos/IF/pages/pageBeco.png">
-			<h1>Em Breve  ...</h1>
+			<div class="row artistas">
+				<div class="col-12 col-sm-12 col-md-12 col-lg-5">
+					<h2>Renata Ventura</h2>
+					<p>Escritora e jornalista</p>
+					<p class="bioConvidados">Jornalista de formação, Renata Ventura trabalhou por três anos com cinema documentário antes de decidir se dedicar exclusivamente à literatura. Lançou "A Arma Escarlate" em novembro de 2011, esgotando nas principais bienais do país e ganhando o Prêmio Codex de Ouro da Literatura, dado aos livros mais bem falados na Internet. Três anos depois, sua continuação, "A Comissão Chapeleira", foi eleita o Melhor Livro Nacional de 2014 pelo Prêmio Juventude Literária. </p>
+					<p class="bioConvidados">Por abordar temas importantes da realidade brasileira e da natureza humana em um ambiente fantástico, seus livros vêm chamando a atenção de jovens e adultos, sendo procurados tanto por seu alto grau de entretenimento quanto por seu conteúdo filosófico, histórico, sociológico e político.</p>
+				</div>
+				<div class="col-12 col-sm-0 col-md-0 col-lg-7">
+					<img src="photos/beco/renata.png">
+				</div>
+			</div>
+			<div class="row artistas">
+				<div class="col-12 col-sm-12 col-md-12 col-lg-5">
+					<h2>Eric Peleas</h2>
+					<p>Diretor de arte, Designer e Ilustrador</p>
+					<p class="bioConvidados">Trabalhei como diretor de arte, designer e ilustrador para empresas como Grande Prêmio Brasil de Fórmula 1, Editora Abril, Greenpeace, World Bank, Sabesp, Médicos Sem Fronteiras, Natura, Editora Trip, Editora Capital Aberto, Governo do Estado do Amazonas, Ingram, entre outros. </p>
+					<p class="bioConvidados">Estreei nos quadrinhos no Festival Internacional de Quadrinhos de Belo Horizonte (FIQ/BH) de 2013, com o lançamento de Ima – Sempre em Frente, a história real de Julia, sobrevivente do holocausto. A graphic novel já vendeu mais de mil exemplares e foi bem recebida por público e crítica.</p>
+				</div>
+				<div class="col-12 col-sm-0 col-md-0 col-lg-7">
+					<img src="photos/beco/eric.png">
+				</div>
+			</div>
+			<div class="row artistas">
+				<div class="col-12 col-sm-12 col-md-12 col-lg-5">
+					<h2>Denis Mello</h2>
+					<p>Quadrinista</p>
+					<p class="bioConvidados">Co-criador de Beladona, HQ vencedora do troféu HQ Mix (2013, 2015, 2016) e sucesso de crítica.Atualmente trabalha em Teocrasília, história que se passa em um futuro próximo, no qual o Brasil foi dominado pela "Bancada da Palavra". </p>
+					<p class="bioConvidados">Desde 2009 produz HQs e ilustrações com temáticas diversas, debutando com uma novela sobre a situação Palestina, passando depois por futebol, humor, polícial, filosofia, drama e etc, até encontrar o terror em Beladona.</p>
+				</div>
+				<div class="col-12 col-sm-0 col-md-0 col-lg-7">
+					<img src="photos/beco/denis.png">
+				</div>
+			</div>
+			<div class="row artistas">
+				<div class="col-12 col-sm-12 col-md-12 col-lg-5">
+					<h2>Rapha Pinheiro</h2>
+					<p>Quadrinista</p>
+					<p class="bioConvidados">Trabalho com quadrinhos desde 2014, desenhando, escrevendo histórias e compartilhando minhas idéias com o mundo. Em 2016 comecei a lecionar um curso livre de desenho e produção de quadrinhos no Colégio Santo Amaro e em 2017 iniciei um canal no Youtube onde compartilho aulas e dicas sobre a nona arte e sua produção. </p>
+					<p class="bioConvidados">Atualmente sou mestrando da Escola de Comunicação da UFRJ, estudando Tecnologias e Linguagens da Comunicação Editorial. Meu projeto desse mestrado se chama Mesa 44  e você deveria conferir.</p>
+				</div>
+				<div class="col-12 col-sm-0 col-md-0 col-lg-7">
+					<img src="photos/beco/rapha.png">
+				</div>
+			</div>
+			<div class="row artistas">
+				<div class="col-12 col-sm-12 col-md-12 col-lg-5">
+					<h2>Pedro Figueiredo</h2>
+					<p>Designer Gráfico e Ilustrador</p>
+					<p class="bioConvidados">Fã de quadrinhos, Star Wars, O Senhor dos Anéis, cultura pop e nerd. Desenha super-heróis e outros personagens desde pequeno, o que fez trilhar o caminho da arte até hoje. </p>
+					<p class="bioConvidados">Formado no IFF Campos dos Goytacazes em Design Gráfico no ano de 2011, trabalhou em algumas agências de publicidade na cidade. Hoje atua como freelancer na área de design e ilustração, sendo a sua verdadeira paixão. Pretende se especializar na área de quadrinhos e criar  uma história de sua autoria.</p>
+				</div>
+				<div class="col-12 col-sm-0 col-md-0 col-lg-7">
+					<img src="photos/beco/pedro.png">
+				</div>
+			</div>
 		</div>		
 	</section>
 
